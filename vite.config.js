f2b2b2b1efcc5f542b2c71f60a540a8546bf8ee7
BANNYJULY <sonialeepy@gmail.com>
2025-06-13 14:14:06 +0900

    123

--- vite.config.js
+++ vite.config.js
@@ -13,23 +13,3 @@ export default defineConfig({
   },
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-  // 모든 nav 메뉴 링크 선택
-  const menuLinks = document.querySelectorAll("nav ul li a");
-
-  menuLinks.forEach(link => {
-      link.addEventListener("click", function (e) {
-          e.preventDefault(); // 기본 동작 방지 (페이지 이동 막기)
-
-          const targetId = this.getAttribute("href").substring(1); // '#home' -> 'home'
-          const targetSection = document.getElementById(targetId);
-
-          if (targetSection) {
-              window.scrollTo({
-                  top: targetSection.offsetTop,
-                  behavior: "smooth"
-              });
-          }
-      });
-  });
-});
